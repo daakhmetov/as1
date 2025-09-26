d0af00a3b50e7aefc8ef1395008ea78340aa64b1
daakhmetov <daurenakhmetov09@gmail.com>
2025-09-26 19:50:06 +0500

    feat(metrics): counters, depth tracker, CSV writer

--- src/main/java/CSVLogger.java
+++ src/main/java/CSVLogger.java
@@ -3,14 +3,23 @@ import java.io.IOException;
 
 public class CSVLogger {
     private final String fileName;
+    private boolean headerWritten = false;
 
     public CSVLogger(String fileName) {
         this.fileName = fileName;
     }
 
-    public void writeRow(int counter, int depth) {
+    public void writeRow(String algo, int n, Metrics metrics, long timeNs) {
         try (FileWriter writer = new FileWriter(fileName, true)) {
-            writer.write(counter + "," + depth + "\n");
+            if (!headerWritten) {
+                writer.write("algo,n,timeNs,comparisons,allocations,maxDepth\n");
+                headerWritten = true;
+            }
+            writer.write(String.format("%s,%d,%d,%d,%d,%d\n",
+                    algo, n, timeNs,
+                    metrics.getComparisons(),
+                    metrics.getAllocations(),
+                    metrics.getMaxDepth()));
         } catch (IOException e) {
             e.printStackTrace();
         }
