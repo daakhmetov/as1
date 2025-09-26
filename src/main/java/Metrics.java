5561493d1598b6affb66d7866d217c7e942431bc
daakhmetov <daurenakhmetov09@gmail.com>
2025-09-26 19:46:25 +0500

    feat(metrics): counters, depth tracker, CSV writer

--- src/main/java/Metrics.java
+++ src/main/java/Metrics.java
@@ -1,26 +1,46 @@
 public class Metrics {
-    private int counter = 0;
+    private long comparisons = 0;
+    private long allocations = 0;
     private int depth = 0;
+    private int maxDepth = 0;
 
-    public void incrementCounter() {
-        counter++;
+    public void incComparisons() {
+        comparisons++;
     }
 
-    public void enterDepth() {
+    public void incAllocations() {
+        allocations++;
+    }
+
+    public void enterRecursion() {
         depth++;
+        if (depth > maxDepth) {
+            maxDepth = depth;
+        }
     }
 
-    public void exitDepth() {
+    public void exitRecursion() {
         if (depth > 0) {
             depth--;
         }
     }
 
-    public int getCounter() {
-        return counter;
+    public long getComparisons() {
+        return comparisons;
     }
 
-    public int getDepth() {
-        return depth;
+    public long getAllocations() {
+        return allocations;
+    }
+
+    public int getMaxDepth() {
+        return maxDepth;
+    }
+
+    public void reset() {
+        comparisons = 0;
+        allocations = 0;
+        depth = 0;
+        maxDepth = 0;
     }
 }
